298009a778395f7fbe30b3785e59af35af1b62de
Zac Edens <themancalledzac@gmail.com>
2021-06-16 14:47:11 -0700

    added background images for photography page tiles

--- src/components/PhotoHome.js
+++ src/components/PhotoHome.js
@@ -5,6 +5,10 @@ import React from "react";
 import colors from "./colors";
 // import { useSelector, useDispatch } from "react-redux";
 import eventImage from "../assets/photography/event/Header1.jpg";
+import landscapeImage from "../assets/photography/landscape/Header1.jpg";
+import portraitImage from "../assets/photography/portrait/Header1.jpg";
+import streetImage from "../assets/photography/street/Header1.jpg";
+import { white } from "kleur";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,7 +17,8 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
-    height: "8rem",
+    height: "10rem",
+    width: "100%",
     color: theme.palette.text.secondary,
   },
   header: {
@@ -30,11 +35,74 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    height: "8rem",
+    height: "10rem",
+    width: "100%",
     "&:hover, &:focus": {
       opacity: "75%",
       cursor: "pointer",
     },
+  },
+  landscape: {
+    padding: theme.spacing(3),
+    backgroundImage: `url(${landscapeImage})`,
+    backgroundPosition: "center",
+    textAlign: "center",
+    position: "relative",
+    color: "colors.white",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "10rem",
+    width: "100%",
+    "&:hover, &:focus": {
+      opacity: "75%",
+      cursor: "pointer",
+    },
+  },
+  portrait: {
+    padding: theme.spacing(3),
+    backgroundImage: `url(${portraitImage})`,
+    backgroundPosition: "center",
+    textAlign: "center",
+    position: "relative",
+    color: "colors.white",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "10rem",
+    width: "100%",
+    "&:hover, &:focus": {
+      opacity: "75%",
+      cursor: "pointer",
+    },
+  },
+  street: {
+    padding: theme.spacing(3),
+    backgroundImage: `url(${streetImage})`,
+    backgroundPosition: "center",
+    textAlign: "center",
+    position: "relative",
+    color: "colors.white",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "10rem",
+    width: "100%",
+    "&:hover, &:focus": {
+      opacity: "75%",
+      cursor: "pointer",
+    },
+  },
+  text: {
+    textAlign: "center",
+    width: "85%",
+    opacity: "70%",
+    backgroundColor: "white",
+    margin: "0 auto",
+    color: "black",
+    position: "relative",
+    left: "0",
+    right: "0",
+    fontSize: "4rem",
+    overflow: "hidden",
+    // mixBlendMode: "screen",
   },
 }));
 // <Grid item xs={12} sm={state.home.photo.gridWidth}>
@@ -57,16 +125,24 @@ export default function PhotoHome(props) {
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <Paper className={(classes.paper, classes.event)}>Event</Paper>
+            <Paper className={(classes.paper, classes.event)}>
+              <h2 className={classes.text}>Event</h2>
+            </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>Portrait</Paper>
+            <Paper className={(classes.paper, classes.portrait)}>
+              <h2 className={classes.text}>Portrait</h2>
+            </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>Street</Paper>
+            <Paper className={(classes.paper, classes.street)}>
+              <h2 className={classes.text}>Street</h2>
+            </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>Landscape</Paper>
+            <Paper className={(classes.paper, classes.landscape)}>
+              <h2 className={classes.text}>Nature</h2>
+            </Paper>
           </Grid>
         </Grid>
       </div>
